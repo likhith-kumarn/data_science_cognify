8bae42bc4cbae8a833f1f1139312c7f9f02e0507
Likhith Kumar N <143432681+likhith-kumarn@users.noreply.github.com>
2026-04-05 14:37:33 +0530

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -9,11 +9,9 @@ df.shape
 df.shape[0]
 df.shape[1]
 df.isnull().sum()
-
 #if null exists
 num_col=df.select_dtypes(include=np.number).columns
 df[num_col]=df[num_col].fillna(df[num_col].median)
-
 cat_col=df.select_dtypes(include=object).columns
 for col in cat_col:
   df[col].fillna(df[col].mode()[0], inplace=True)
